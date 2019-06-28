fc09eda7e2e0278339aeb837b34e94d170f9a6f5
cscyuge <32408175+cscyuge@users.noreply.github.com>
2019-06-28 13:57:54 +0800

    part1

---
+++ src/computer/Computer.java
@@ -0,0 +1,77 @@
+package computer;
+
+public abstract class Computer {
+    
+    private String name;
+    private String brand;
+    private String cpu;
+    private String memory;
+    private String hardDisk;
+    private String monitor;
+    
+    
+    public Computer() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public Computer(String name,String brand,String cpu,String memory, String hardDisk,String monitor) {
+        this.name=name;
+        this.brand=brand;
+        this.cpu=cpu;
+        this.memory=memory;
+        this.hardDisk=hardDisk;
+        this.monitor=monitor;
+    }
+    
+    public abstract void printInfo();
+ 
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public String getHardDisk() {
+        return hardDisk;
+    }
+
+    public void setHardDisk(String hardDisk) {
+        this.hardDisk = hardDisk;
+    }
+
+    public String getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(String monitor) {
+        this.monitor = monitor;
+    }   
+}
